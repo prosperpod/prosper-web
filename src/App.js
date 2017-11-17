85baad32cfa2a98412b13f73a06b1dd923847b79
Steven Xie <steven.xie@outlook.com>
2017-11-17 16:06:00 -0500

    Added new styling, added websocket interactions.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Configuration from './shared/Configuration';
 
 export default class App extends Component {
   constructor(props) {
@@ -10,9 +11,71 @@ export default class App extends Component {
       },
       console: {
         elements: [],
+        prefixes: {
+          usr: "&!:",
+          sys: "$:",
+        }
       },
-      modePrefix: "testing",
+      socket: {
+        open: false,
+      },
     }
+  }
+
+  componentDidMount() {
+    this.socket = new WebSocket(Configuration.serverSocketAddress);
+    this.socket.addEventListener('open', (event) => {
+      this.socket.send("connection-did-open");
+      this.setState((state) => {
+        state.socket.open = true;
+        return state;
+      });
+    });
+    this.socket.addEventListener('message', (event) => {
+      this.handleServerMessage(event.data);
+    });
+    this.socket.addEventListener('error', (event) => {
+      this.handleSocketError(event);
+    });
+    this.socket.addEventListener('close', (event) => {
+      console.log(event);
+      this.addConsoleElement("sys", "Socket connection closed" +
+          (event.reason ? (": " + event.reason) : ", reason unknown."), "Alert:")
+      this.setState((state) => {
+        state.socket.open = false;
+        return state;
+      });
+    });
+  }
+
+  handleServerMessage(msg) {
+    try {
+      const json = JSON.parse(msg);
+      if (json.hasOwnProperty("messages")) {
+        json.messages.forEach((message) => {
+          this.addConsoleElement("sys", message, json.prefix);
+        });
+      } else if (json.hasOwnProperty("message")) {
+        this.addConsoleElement("sys", json.message, json.prefix);
+      }
+
+      if (json.hasOwnProperty("console-command")) {
+        const command = json["console-command"];
+        if (command === "clear-console") {
+          this.clearConsole();
+        }
+      }
+    } catch (err) {
+      if (err.name === "SyntaxError") {
+        this.addConsoleElement("sys", msg);
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  handleSocketError(event) {
+    this.addConsoleElement("sys", "A socket error occurred...", "Error:");
   }
 
   handleInputChange(event) {
@@ -25,11 +88,22 @@ export default class App extends Component {
 
   handleInputSubmit(event) {
     event.preventDefault();
-    this.addConsoleElement("usr", this.state.input.value);
+    const message = this.state.input.value;
+    this.addConsoleElement("usr", message);
+    this.sendSocketMessage(message);
     this.setState((state) => {
       state.input.value = "";
       return state;
     });
+  }
+
+  sendSocketMessage(msg) {
+    if (this.state.socket.open) {
+      this.socket.send(msg);
+    } else {
+      this.addConsoleElement("sys", "Socket connection could not be made, no communication" +
+          " possible.", "Warning:");
+    }
   }
 
   clearConsole() {
@@ -39,17 +113,26 @@ export default class App extends Component {
     })
   }
 
-  addConsoleElement(src, msg) {
+  addConsoleElement(src, msg, prefix) {
+    if (prefix == null) {
+      if (src === "usr") {
+        prefix = this.state.console.prefixes.usr;
+      } else {
+        prefix = this.state.console.prefixes.sys;
+      }
+    }
+
     this.setState((state) => {
       if (state.console.elements.length === 0) {
-        state.console.elements = [{id: 1, src: src, msg: msg}];
+        state.console.elements = [{id: 1, src: src, msg: msg, prefix: prefix}];
       } else {
         const id = state.console.elements[0].id + 1;
-        state.console.elements.unshift({id: id, src: src, msg: msg});
+        state.console.elements.unshift({id: id, src: src, msg: msg, prefix: prefix});
         if (state.console.elements.length > 300) {
           state.console.elements = state.console.elements.slice(0, 300);
         }
       }
+      return state;
     });
   }
 
@@ -57,7 +140,7 @@ export default class App extends Component {
     const elems = this.state.console.elements;
     return elems.map(elem =>
         <li key={elem.id} className={elem.src}>
-          <p className={"prefix"}>{this.state.modePrefix}</p>
+          <p className={"prefix"}>{elem.prefix}</p>
           <p className={"message"}>{elem.msg}</p>
         </li>
     );
---
+++ src/shared/Configuration.js
@@ -0,0 +1,3 @@
+export default {
+  serverSocketAddress: "ws://core.prosperpod.ca:8080/prosper-core/"
+}
